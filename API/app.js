71aee7c8c562da587d85954849e5d60f70af92e2
Javier Leyva Seiglie <96433186+jseiglie@users.noreply.github.com>
2022-08-03 01:20:23 +0200

    cors added

--- API/app.js
+++ API/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require("path");
-const Posts = require('./Models/Posts')
-const Catego = require('./Models/Catego')
+const Posts = require('./Models/Posts');
+const Catego = require('./Models/Catego');
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors())
 
 //database
 const db = require("./config/database");
